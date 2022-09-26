7560785a8e4711ff5871d4ee432f99ad4734fd82
MollaMandalin <92661445+MollaMandalin@users.noreply.github.com>
2022-09-26 21:56:59 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -40,9 +40,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "86a2b2b60f6a244aaeac6b732a0b2303",
-"/": "86a2b2b60f6a244aaeac6b732a0b2303",
-"main.dart.js": "39189c06e0843c144214223fe3eee377",
+"index.html": "75d8823890c52c95bc799078c2f659a3",
+"/": "75d8823890c52c95bc799078c2f659a3",
+"main.dart.js": "f0d32f9bd08f883d0b05501419d1d2b5",
 "manifest.json": "d8fe34f7ae4c072a77b924e01dac8a50",
 "version.json": "9b818ca9511483c901bed1545384376c"
 };
